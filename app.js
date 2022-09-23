ff8187e35b76ef7a8d70683cee13b254f57336cf
musatafa-mehboob <musatafamehboob996@gmail.com>
2022-09-23 17:45:35 +0500

    update data

--- app.js
+++ app.js
@@ -1,19 +1,119 @@
+// let newsValue = document.getElementById("newsValue");
+
+// if (!newsValue.value) {
+//     newsValue.value = "karachi"
+//     console.log(newsValue.value);
+// }
+
+// const getData = new Promise((resolve, reject) => {
+//     fetch(
+//         "https://newsapi.org/v2/everything?q=" + newsValue.value + "&apiKey=820f631351e64ec3a69317b05c1ea811"
+//     )
+//         .then((res) => res.json())
+//         .then((res) => resolve(res))
+//         .catch((err) => {
+//             reject(err)
+//             swal("Enter avalid value")
+//         });
+// });
+
+// getData.then((res) => {
+//     console.log(res);
+
+
+//     let articles = res.articles;
+//     console.log(res.articles.length);
+//     for (var i = 0; i < articles.length; i++) {
+//         console.log(articles);
+//         var img = `
+//         <div class="single-card-div " >
+//              <img src="${articles[i].urlToImage}" class="card-img-top" alt="...">
+//              <div class="card-body">
+//                  <h5 class="card-title">${articles[i].title}</h5>
+//                  <p class="card-text">${articles[i].description}</p>
+//                  <a href="${articles[i].url}" class="btn btn-primary">Read more</a>
+//              </div>
+//         </div>
+//         `;
+//         document.getElementById("news-data").innerHTML += img
+//     }
+
+
+//     getData.catch((err) => {
+//     })
+
+
+// }
+
+// );
+
+
+// function search() {
+//     console.log(newsValue.value);
+
+
+
+//         const getData = new Promise((resolve, reject) => {
+//             fetch(
+//                 "https://newsapi.org/v2/everything?q=" + newsValue.value + "&apiKey=820f631351e64ec3a69317b05c1ea811"
+//             )
+//                 .then((res) => res.json())
+//                 .then((res) => resolve(res))
+//                 .catch((err) => {
+//                     reject(err)
+//                     swal("Enter avalid value")
+//                 });
+//         });
+
+//         getData.then((res) => {
+//             console.log(res);
+
+
+//             let articles = res.articles;
+//             console.log(res.articles.length);
+//             for (var i = 0; i < articles.length; i++) {
+//                 console.log(articles);
+//                 var img = `
+//                 <div class="single-card-div " >
+//                      <img src="${articles[i].urlToImage}" class="card-img-top" alt="...">
+//                      <div class="card-body">
+//                          <h5 class="card-title">${articles[i].title}</h5>
+//                          <p class="card-text">${articles[i].description}</p>
+//                          <a href="${articles[i].url}" class="btn btn-primary">Read more</a>
+//                      </div>
+//                 </div>
+//                 `;
+//                 document.getElementById("news-data").innerHTML += img
+//             }
+
+
+//             getData.catch((err) => {
+//             })
+
+
+//         }
+
+//         );
+
+
+// }
+
+
+
+
+
+
+
 let newsValue = document.getElementById("newsValue");
 
-function search() {
+if (!newsValue.value) {
+    newsValue.value = "karachi"
     console.log(newsValue.value);
-
-    if (!newsValue.value) {
-        newsValue = "karachi"
-    } else {
-        
-    }
-
-
+}
 
 const getData = new Promise((resolve, reject) => {
     fetch(
-        "https://newsapi.org/v2/everything?q="+newsValue.value+"i&apiKey=820f631351e64ec3a69317b05c1ea811"
+        "https://newsapi.org/v2/everything?q=" + newsValue.value + "&apiKey=820f631351e64ec3a69317b05c1ea811"
     )
         .then((res) => res.json())
         .then((res) => resolve(res))
@@ -25,33 +125,97 @@ const getData = new Promise((resolve, reject) => {
 
 getData.then((res) => {
     console.log(res);
-    
+
+
+    let articles = res.articles;
+    console.log(res.articles.length);
+
+    var newsData = document.getElementById("news-data")
+    newsData = ""
+    for (var i = 0; i < 1; i++) {
+        console.log(articles);
+        var img = `
+                        <div class="single-card-div " >
+                             <img src="${articles[i].urlToImage}" class="card-img-top" alt="...">
+                             <div class="card-body">
+                                 <h5 class="card-title">${articles[i].title}</h5>
+                                 <p class="card-text">${articles[i].description}</p>
+                                 <a href="${articles[i].url}" class="btn btn-primary">Read more</a>
+                             </div>
+                        </div>
+                        `;
+        document.getElementById("news-data").innerHTML += img
+    }
+
+
+
+
+    getData.catch((err) => {
+    })
+
+
+}
+
+);
+
+
+
+function search() {
+    console.log(newsValue.value);
+
+    const getData = new Promise((resolve, reject) => {
+        fetch(
+            "https://newsapi.org/v2/everything?q=" + newsValue.value + "&apiKey=820f631351e64ec3a69317b05c1ea811"
+        )
+            .then((res) => res.json())
+            .then((res) => resolve(res))
+            .catch((err) => {
+                reject(err)
+                swal("Enter avalid value")
+            });
+    });
+
+    getData.then((res) => {
+        console.log(res);
 
         let articles = res.articles;
         console.log(res.articles.length);
-        for (var i = 0; i < articles.length; i++) {
+
+        const el = document.querySelector(".single-card-div");
+
+Object.keys(el.dataset).forEach(key=>{
+    delete el.dataset[key];
+})
+
+        console.log(articles);
+        // newsData.innerHTML = ""
+        // img = ""
+        newsData = document.getElementById("news-data")
+        for (var i = 0; i < 1; i++) {
             console.log(articles);
             var img = `
-            <div class="single-card-div " >
-                 <img src="${articles[i].urlToImage}" class="card-img-top" alt="...">
-                 <div class="card-body">
-                       <h5 class="card-title">${articles[i].author}</h5>
-                     <h5 class="card-title">${articles[i].title}</h5>
-                     <p class="card-text">${articles[i].description}</p>
-                     <a href="${articles[i].url}" class="btn btn-primary">Read more</a>
-                 </div>
-            </div>
-            `;
+                <div class="single-card-div " >
+                    <img src="${articles[i].urlToImage}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${articles[i].title}</h5>
+                            <p class="card-text">${articles[i].description}</p>
+                            <a href="${articles[i].url}" class="btn btn-primary">Read more</a>
+                        </div>
+                </div>
+                            `;
             document.getElementById("news-data").innerHTML += img
         }
+        console.log(newsData.length);
+
 
 
         getData.catch((err) => {
         })
 
-        
-}
-  
-);
+
+    }
+
+    );
+
 
 }
